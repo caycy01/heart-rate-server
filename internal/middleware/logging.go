6cef4a882237a92f0260aad754b847a703b305c7
xihan123 <srxqzxs@vip.qq.com>
2025-04-03 14:59:29 +0800

    fix(build): 优化 JSONContentTypeMiddleware 以支持静态资源

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -4,6 +4,7 @@ import (
 	"heart-rate-server/internal/utils"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -29,6 +30,10 @@ func RecoveryMiddleware(next http.Handler) http.Handler {
 
 func JSONContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/static/") {
+			next.ServeHTTP(w, r)
+			return
+		}
 		w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
